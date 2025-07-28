4cf7796ca8d403978f11683216d3b11f020ffb33
Ashish Kumar Sahu <ashish.omm2005@gmail.com>
2025-07-28 22:11:30 +0530

    Fixed some UI bugs

--- client/src/Components/CardSection.jsx
+++ client/src/Components/CardSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CardSection = () => {
   return (
-    <div className="flex flex-row items-center justify-between flex-wrap max-w-6xl px-2 py-8 rounded-3xl mx-auto space-y-6">
+    <div className="flex flex-row items-center justify-between flex-wrap max-w-6xl px-2 py-8 rounded-3xl mx-auto max-md:space-y-6">
       <div className="flex items-center flex-1/2 space-x-2 text-shadow-7xs text-shadow-[#300CAB] px-4">
         <div className="flex justify-center items-center flex-1/2 space-x-2 text-shadow-7xs text-shadow-[#300CAB]">
           <h1 className="text-3xl md:text-5xl font-extrabold gradient">7+</h1>{" "}
@@ -19,7 +19,7 @@ const CardSection = () => {
       </div>
       <div className="flex items-center flex-1/2 space-x-2 text-shadow-7xs text-shadow-[#300CAB] px-4">
         <div className="flex justify-center items-center flex-1/2 space-x-2 text-shadow-7xs text-shadow-[#300CAB]">
-          <h1 className="text-3xl font-extrabold gradient">500+</h1>{" "}
+          <h1 className="text-3xl md:text-5xl font-extrabold gradient">500+</h1>{" "}
           <span className="text-md font-medium gradient2">
             Problems <br /> Solved
           </span>
